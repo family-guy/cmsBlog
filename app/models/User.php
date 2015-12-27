cb25c9627036f97d9e44822c9a706c54e7b2ddc6
family-guy <grking8@gmail.com>
2015-12-27 21:45:45 +0000

    comments edits

--- app/models/User.php
+++ app/models/User.php
@@ -209,14 +209,14 @@ class User {
 	* @param Db object <code>$db</code>, array of strings <code>$cols</code>, string <code>$order</code>, integer <code>$nb_results</code>, <code>boolean</code> $desc
 	* @return array of associative arrays; keys are column headings or null if zero rows returned
 	*/
-	private static function retrieve(Db $db, $cols, $order, $nb_results, $desc) {
+	public static function retrieve(Db $db, $cols, $order, $nb_results, $desc) {
 		return $db->retrieve($cols, $order, $nb_results, self::table, $desc);
 	}
 	/**
 	* @param Db object <code>$db</code>, array of strings <code>$cols</code>, string <code>$cond</code>
 	* @return array of associative arrays; keys are column headings or null if zero rows returned
 	*/
-	private static function find(Db $db, $cols, $cond) {
+	public static function find(Db $db, $cols, $cond) {
 		return $db->find($cols, $cond, self::$table);
 	}
 	/**
